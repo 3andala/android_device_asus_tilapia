15d79192555de0480ecd9dd94d533e9acfa74ca8
3andala <love3andala@gmail.com>
2014-05-25 06:01:21 +0300

    Update and rename cm.mk to nameless_tilapia.mk

--- nameless_tilapia.mk
+++ nameless_tilapia.mk
@@ -1,31 +1,23 @@
-# Release name
-PRODUCT_RELEASE_NAME := Nexus7-GSM
+# Inherit APNs list
+$(call inherit-product, vendor/nameless/config/apns.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 800
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/nameless/config/common.mk)
 
-# Inherit telephony common stuff
-$(call inherit-product, vendor/cm/config/telephony.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/asus/tilapia/full_tilapia.mk)
 
 ## Device identifier. This must come after all inclusions
+PRODUCT_RELEASE_NAME := Nexus7-GSM
 PRODUCT_DEVICE := tilapia
-PRODUCT_NAME := cm_tilapia
+PRODUCT_NAME := nameless_tilapia
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 7
 PRODUCT_MANUFACTURER := Asus
-
-# Custom unofficial build tag
-TARGET_UNOFFICIAL_BUILD_ID := temasek
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasig BUILD_FINGERPRINT="google/nakasig/tilapia:4.4/KRT16S/920375:user/release-keys" PRIVATE_BUILD_DESC="nakasig-user 4.4 KRT16S 920375 release-keys"
